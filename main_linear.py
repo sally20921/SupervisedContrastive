fba2cab636121497a1c87f1ef0e2ffd54e056fb4
sally20921 <sally20921@github.com>
2020-07-04 09:21:27 +0900

    main_linear.py

--- main_linear.py
+++ main_linear.py
@@ -6,7 +6,10 @@ import time
 import math
 
 import torch
-import torch.backends.cudnn as cudnn
+
+from util import adjust_learning_rate, warmup_learning_rate, accuracy
+from util import set_optimizer
+from networks.resnet_big import SupConResNet, LinearClassifier
 
 def parse_option():
     parser = argparse.ArgumentParser('argument for training')
@@ -20,4 +23,35 @@ def parse_option():
 
 
 
+def set_model(opt):
+    model = SupConResNet(name=opt.name)
+    criterion = torch.nn.CrossEntropyLoss()
+    classifier = LinearClassifier(name=opt.model, num_classes=opt.n_cls)
 
+    ckpt = torch.load(opt.ckpt, map_location='cpu')
+    state_dict = ckpt['model']
+
+    return model, classifier, criterion 
+
+def train(train_loader, model, classifier, criterion, optimizer, epoch, opt):
+    #one epoch training 
+    model.eval()
+    classifier.train()
+
+def main():
+    opt = parse_option()
+
+    train_loader, val_loader = set_loader(opt)
+
+    model, classifier, criterion = set_model(opt)
+
+    for epoch in range(1, opt.epochs+1):
+        #train for one epoch 
+        loss = train(train_loader, model, classifier, criterion, optimizer, epoch, opt)
+        
+        #eval for one epoch 
+        loss =  validate(val_loader, model, classifier, criterion, opt)
+
+
+if __name__=='__main__':
+    main()
