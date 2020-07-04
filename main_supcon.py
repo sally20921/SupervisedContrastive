b4edca503759ec8cf564de3936d859586a74f0ae
sally20921 <sally20921@github.com>
2020-07-04 09:14:31 +0900

    main_supcon

--- main_supcon.py
+++ main_supcon.py
@@ -10,6 +10,16 @@ import tensorboard_logger as tb_logger
 import torch
 import torch.backends.cudnn as cudnn
 from torchvision import transforms, datasets
+def set_model(opt):
+    model = SupConResNet(name=opt.model)
+    #supervised contrastive loss 
+    criterion = SupConLoss(temperature=opt.temp)
+    
+    return model, criterion
+
+def train(train_loader, model, criterion, optimizer, epoch, opt):
+    #one epoch training
+    model.train()
 
 def main():
     opt =  parse_option()
