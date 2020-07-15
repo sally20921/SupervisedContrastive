4de35b5936e0ba43ff78ab18c6113d27ef47e614
Seri Lee <sally20921@snu.ac.kr>
2020-07-15 19:06:47 +0900

    Update resnet_big.py

--- networks/resnet_big.py
+++ networks/resnet_big.py
@@ -1,3 +1,9 @@
+"""ResNet in PyTorch.
+ImageNet-Style ResNet
+[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+    Deep Residual Learning for Image Recognition. arXiv:1512.03385
+Adapted from: https://github.com/bearpaw/pytorch-classification
+"""
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -155,35 +161,50 @@ class LinearBatchNorm(nn.Module):
         x = x.view(-1, self.dim)
         return x
 
+
 class SupConResNet(nn.Module):
-    def __init__(self, name='resnet50', feat_dim=128):
+    """backbone + projection head"""
+    def __init__(self, name='resnet50', head='mlp', feat_dim=128):
         super(SupConResNet, self).__init__()
-
         model_fun, dim_in = model_dict[name]
-
-        self.encoder  = model_fun()
-
-        self.head = nn.Sequential(
+        self.encoder = model_fun()
+        if head == 'linear':
+            self.head = nn.Linear(dim_in, feat_dim)
+        elif head == 'mlp':
+            self.head = nn.Sequential(
                 nn.Linear(dim_in, dim_in),
                 nn.ReLU(inplace=True),
                 nn.Linear(dim_in, feat_dim)
             )
+        else:
+            raise NotImplementedError(
+                'head not supported: {}'.format(head))
 
     def forward(self, x):
         feat = self.encoder(x)
         feat = F.normalize(self.head(feat), dim=1)
-        return feat 
+        return feat
+
+
+class SupCEResNet(nn.Module):
+    """encoder + classifier"""
+    def __init__(self, name='resnet50', num_classes=10):
+        super(SupCEResNet, self).__init__()
+        model_fun, dim_in = model_dict[name]
+        self.encoder = model_fun()
+        self.fc = nn.Linear(dim_in, num_classes)
+
+    def forward(self, x):
+        return self.fc(self.encoder(x))
 
 
 class LinearClassifier(nn.Module):
-    def __init__(self, name = 'resnet50', num_classes=10):
+    """Linear classifier"""
+    def __init__(self, name='resnet50', num_classes=10):
         super(LinearClassifier, self).__init__()
         _, feat_dim = model_dict[name]
         self.fc = nn.Linear(feat_dim, num_classes)
 
     def forward(self, features):
-        return self.fc(features) 
+        return self.fc(features)
 
-        
-
-        
